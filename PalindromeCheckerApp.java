789e1e328b99333bafec676436209e22ce968751
Akshat Agarwal <aa8245@srmist.edu.in>
2026-02-27 15:54:50 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,7 +1,27 @@
 package Palindrome_Checker_App;
 
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("Hello!!!");
+   public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        boolean isPalindrome = true;
+
+        // Check palindrome
+        for (int i = 0; i < word.length() / 2; i++) {
+
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
     }
 }
